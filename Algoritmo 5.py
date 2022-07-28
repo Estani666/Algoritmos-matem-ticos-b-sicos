6858bac59690ed860e53959706a4f656a2fefa06
Estanislao Talenti Lopez <89042090+Estani666@users.noreply.github.com>
2022-07-28 16:55:14 -0300

    Update Algoritmo 5.py

--- Algoritmo 5.py
+++ Algoritmo 5.py
@@ -1,13 +1,14 @@
 #Algorítmo 5: Muestra los números primos en un determinado rango.
 
-potentially_prime = int(input("Ingrese un número: "))
 def is_prime(potentially_prime):
-    for i in range(2, potentially_prime): #range(inicio, fin) va desde inicio a fin-1
+    for i in range(2, potentially_prime // 2 + 1):
         if potentially_prime % i == 0:
             return False
-        return True
-primos = [2, ]
-for i in range(2, potentially_prime):
-    if is_prime(i):
-        primos.append(i)
-print(primos)
+    return True
+primes = []
+valor = int(input("Digite un número entero: "))
+for number in range(2, valor):
+    if is_prime(number):
+        primes.append(number)
+print("Los números primos hasta el valor digitado son: ")
+print(primes)
