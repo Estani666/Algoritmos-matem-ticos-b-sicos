dddb9d4b0730707bd52f5cbd8b572932912bc39a
Estanislao Talenti Lopez <89042090+Estani666@users.noreply.github.com>
2022-08-19 07:22:19 -0300

    Update De_numeros_romanos_a_numeros_arabigos.py

--- De_numeros_romanos_a_numeros_arabigos.py
+++ De_numeros_romanos_a_numeros_arabigos.py
@@ -1,3 +1,5 @@
+#El siguiente es un programa que convierte un número romano dado en su equivalente arábigo.
+
 valor_arabigo = {"I": 1, "V": 5, "X": 10, "L": 50,
                 "C": 100, "D": 500, "M": 1000}
 numero_romano = input("Ingrese un número romano: ")
